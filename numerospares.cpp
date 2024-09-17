633c0a2ade09e1310315165ca898bd0a5307706d
hckdres <160516217+hckdres@users.noreply.github.com>
2024-09-17 17:12:53 -0500

    Create numerospares.cpp

---
+++ numerospares.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+int a;
+  cout<<"Enter the value of a for you know is par"<<endl;
+  cin>>a;
+  if(a%3==0||a%5==0){
+  cout<<"The number is impar"<<endl;
+  }
+   else if(a%2==0){
+    cout<<"The number is par"<<endl;
+  }
+
+  
+}
